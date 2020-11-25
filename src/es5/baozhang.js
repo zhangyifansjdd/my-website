7fda1d7ecee3058ce16241cdc7782d39528d509d
ZhangYifan <zhangyifansjdd@163.com>
2020-11-25 16:35:22 +0800

    配置

--- src/es5/baozhang.js
+++ src/es5/baozhang.js
@@ -1,20 +1,2 @@
 console.log('开始工具')
 
-class aaa{
-
-}
-
-function sleep(ms) {
-  return new Promise(resolve =>
-    setTimeout(resolve, ms)
-  )
-};
-
-async function start(){
-  await sleep(3000);
-}
-
-start()
-  .then(()=>{
-    console.log('aaaaaaa');
-  });
