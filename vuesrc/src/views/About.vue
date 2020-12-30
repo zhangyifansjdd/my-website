4bc1fc049c656e143b1680059fd8f73ddc9be3e7
ZhangYifan <zhangyifansjdd@163.com>
2020-12-30 17:52:44 +0800

    添加about测试代码

--- vuesrc/src/views/About.vue
+++ vuesrc/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div style="font-size: 30px">
+    <button style="margin: 10px" @click="test">按钮1</button>
+    <button style="margin: 10px" @click="test1">按钮2</button>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "Test",
+    mounted() {
+      setTimeout(() => {
+        window.location.href = 'weixin://dl/business/?ticket=lec1d8be1ec4ebfa2ec97ed884d0d844e';
+      }, 2000)
+    },
+    methods: {
+      test() {
+        window.location.href = 'weixin://dl/business/?ticket=lec1d8be1ec4ebfa2ec97ed884d0d844e';
+      },
+      test1() {
+        window.location.href = 'alipays://platformapi/startapp?appId=2018112062226732&page=pages/newhome/newhome&query=alipay_msg_channel_id=template&alipay_msg_type=hby4q';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
