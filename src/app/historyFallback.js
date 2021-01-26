7b7c96907072739138015cb59c20e4702542cbd3
ZhangYifan <zhangyifansjdd@163.com>
2021-01-26 17:59:03 +0800

    修复

--- src/app/historyFallback.js
+++ src/app/historyFallback.js
@@ -1,16 +1,14 @@
 module.exports = async (ctx, next) => {
   await next();
   if (ctx.status === 404 && ctx.request.headers['accept'].includes('text/html')) {
-    // if (ctx.url.startsWith('/v3') > -1) {
-    //   ctx.status = 200;
-    //   try {
-    //     await ctx.render('/dist-vue3/index.html');
-    //   } catch (e) {
-    //     console.error(ctx.url,e);
-    //   }
-    // } else
-      if (ctx.url.startsWith('/') > -1) {
-      console.log('render');
+    if (ctx.url.startsWith('/v3')) {
+      ctx.status = 200;
+      try {
+        await ctx.render('/dist-vue3/index.html');
+      } catch (e) {
+        console.error(ctx.url,e);
+      }
+    } else if (ctx.url.startsWith('/')) {
       ctx.status = 200;
       try {
         await ctx.render('/dist/index.html');
